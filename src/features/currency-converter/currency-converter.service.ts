fd0371a2ad3996ad970de0ee09de20eca9fb1b99
benhassen <benhassen@brainstack.net>
2022-03-16 16:29:26 +0100

    fix in conversion api

--- src/features/currency-converter/currency-converter.service.ts
+++ src/features/currency-converter/currency-converter.service.ts
@@ -6,19 +6,16 @@ import { ConverterResponse } from "./definitions";
 const { CURRENCY_CONVERTER_BASE_API, FIXER_API_ACCESS } = config;
 
 const convertCurrency = async (from: string, to: string, amount: number) => {
-    console.log("CURRENCY_CONVERTER_BASE_API",CURRENCY_CONVERTER_BASE_API, from , to , amount)
   try {
     const result = await axios.get(
       CURRENCY_CONVERTER_BASE_API +
         "latest" +
         "?access_key=" +
         FIXER_API_ACCESS +
-        "&base=" +
-        from +
         "&symbols=" +
         `${from},${to}`
     );
-    console.log("result.data",  result.data, FIXER_API_ACCESS)
+    
     const { rates, success, date }: ConverterResponse = result.data;
     const convertedAmount = rates[to] * amount;
     return {
